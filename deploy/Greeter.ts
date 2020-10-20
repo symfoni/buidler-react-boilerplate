0249de75df0cb67fe0b8ded411652d39d60f6458
Robin Pedersen <robin@roberto.local>
2020-10-20 09:24:48 +0200

    First commit

---
+++ deploy/Greeter.ts
@@ -0,0 +1,15 @@
+import {
+  BuidlerRuntimeEnvironment,
+  DeployFunction,
+} from "@nomiclabs/buidler/types";
+
+const func: DeployFunction = async function(bre: BuidlerRuntimeEnvironment) {
+  const { deploy } = bre.deployments;
+  const { deployer } = await bre.getNamedAccounts();
+  await deploy("Greeter", {
+    from: deployer,
+    // gas: 4000000,
+    args: ["Hello from Buidler React!"],
+  });
+};
+export default func;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BuidlerContext } from "./buidler/BuidlerContext";
+import { Greeter } from './components/Greeter';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BuidlerContext>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+          <Greeter></Greeter>
+        </BuidlerContext>
+      </header>
+    </div>
+  );
+}
+
+export default App;
